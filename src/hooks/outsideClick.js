b52c4265dc73d948037e79bad25f91780cea0062
Manish <iammanishshrma@gmail.com>
2023-09-01 23:28:04 +0530

    first commit

---
+++ src/hooks/outsideClick.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+//* Custom hook: Takes ref of html element and callback function.
+//* It checks if user clicked outside ref element and calls callback accordingly.
+
+export const useOutsideClick = (ref, handler) => {
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+
+      handler(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+};
